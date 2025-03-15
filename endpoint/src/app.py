d1b4c052178b32e792f0d70891ce1754e051e064
JivkoNushev <jivkon1410@gmail.com>
2025-03-15 21:40:24 +0200

    more apis

--- endpoint/src/app.py
+++ endpoint/src/app.py
@@ -27,5 +27,41 @@ def get_whitelisted(device_ip):
     except Exception as e:
         return str(e)
 
+@app.route('/get_logs', methods=['POST'])
+def get_logs():
+    try:
+        cursor.execute("SELECT * FROM Logs")
+        result = cursor.fetchall()
+        return jsonify(result)
+    except Exception as e:
+        return str(e)
+    
+@app.route('/get_devices', methods=['POST'])
+def get_devices():
+    try:
+        cursor.execute("SELECT * FROM Devices")
+        result = cursor.fetchall()
+        return jsonify(result)
+    except Exception as e:
+        return str(e)
+
+@app.route('/get_device', methods=['POST'])
+def get_device(device_ip):
+    try:
+        cursor.execute("SELECT * FROM Devices WHERE ip = {device_ip}")
+        result = cursor.fetchall()
+        return jsonify(result)
+    except Exception as e:
+        return str(e)
+
+@app.route('/get_device_logs', methods=['POST'])
+def get_device_logs(device_ip):
+    try:
+        cursor.execute("SELECT * FROM Logs WHERE ip_src = {device_ip} OR ip_dst = {device_ip}")
+        result = cursor.fetchall()
+        return jsonify(result)
+    except Exception as e:
+        return str(e)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
