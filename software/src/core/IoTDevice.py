d34c23f56ffc2683c46de6a9d5c703c9921f54d1
JivkoNushev <jivkon1410@gmail.com>
2025-03-15 21:44:14 +0200

    remove redundant code

--- software/src/core/IoTDevice.py
+++ software/src/core/IoTDevice.py
@@ -21,7 +21,6 @@ class IoTDevice:
 
         self.is_quarantined = False
 
-
     def update_from_packet(self, packet):
         if packet.port not in self.ports:
             self.ports.append(packet.port)
@@ -42,87 +41,6 @@ class IoTDevice:
 
     def remove_from_whitelist(self, ip):
         self.whitelist.remove(ip)
-
-    def _block_traffic(self, direction):
-        if direction.upper() != 'INPUT' and direction.upper() != 'OUTPUT':
-            print('Invalid direction')
-            return
-
-        for ip in [device.ip for device in self.whitelist]:
-            rule = iptc.Rule()
-            
-            if direction.upper() == 'INPUT':
-                rule.in_interface = INTERFACE
-                rule.src = ip
-            else:  # OUTPUT
-                rule.out_interface = INTERFACE
-                rule.dst = ip
-            
-        target = iptc.Target(rule, "ACCEPT")
-        rule.target = target
-        chain.insert_rule(rule)  # Insert at beginning to ensure priority
-
-        
-        chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), direction.upper())
-        rule = iptc.Rule()
-        rule.in_interface = INTERFACE
-        rule.src = self.ip
-        rule.target = iptc.Target(rule, 'DROP')
-        chain.insert_rule(rule)
-        
-    
-    def _unblock_traffic(self, direction):
-        if direction.upper() != 'INPUT' and direction.upper() != 'OUTPUT':
-            print('Invalid direction')
-            return
-
-        chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), direction.upper())
-        rule = iptc.Rule()
-        rule.in_interface = INTERFACE
-        rule.src = self.ip
-        rule.target = iptc.Target(rule, 'ACCEPT')
-        chain.insert_rule(rule)
-
-    def _block_all_traffic(self, keep_whitelisted: bool = True):
-        # remove all rules
-        self._block_traffic('INPUT')
-        self._block_traffic('OUTPUT')
-
-    def _unblock_all_traffic(self):
-        self._unblock_traffic('INPUT')
-        self._unblock_traffic('OUTPUT')
-
-    def _block_traffic_to(self, ip):
-        chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), 'OUTPUT')
-        rule = iptc.Rule()
-        rule.in_interface = INTERFACE
-        rule.dst = ip
-        rule.target = iptc.Target(rule, 'DROP')
-        chain.insert_rule(rule)
-    
-    def _unblock_traffic_to(self, ip):
-        chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), 'OUTPUT')
-        rule = iptc.Rule()
-        rule.in_interface = INTERFACE
-        rule.dst = ip
-        rule.target = iptc.Target(rule, 'ACCEPT')
-        chain.insert_rule(rule)
-
-    def _block_traffic_from(self, ip):
-        chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), 'INPUT')
-        rule = iptc.Rule()
-        rule.in_interface = INTERFACE
-        rule.src = ip
-        rule.target = iptc.Target(rule, 'DROP')
-        chain.insert_rule(rule)
-    
-    def _unblock_traffic_from(self, ip):
-        chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), 'INPUT')
-        rule = iptc.Rule()
-        rule.in_interface = INTERFACE
-        rule.src = ip
-        rule.target = iptc.Target(rule, 'ACCEPT')
-        chain.insert_rule(rule)
 
     def __str__(self):
         return f'IP: {self.ip}, MAC: {self.mac_address}'
